b63258b1fc330f0ccdf06f83af48a91c3be7e8ff
Samuel Drilard <wargzord@gmail.com>
2020-02-19 02:10:33 -0300

    api de campanhas funcionando

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('register', 'Auth\RegisterController@register');
- 
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout');
+
+// Route::group(['middleware' => 'auth:api'], function() {
+//     Route::get('campanhas', 'CampanhasController@index');
+//     Route::get('campanhas/{campanha}', 'CampanhasController@show');
+//     Route::post('campanhas', 'CampanhasController@store');
+//     Route::put('campanhas/{campanha}', 'CampanhasController@update');
+//     Route::delete('campanhas/{campanha}', 'CampanhasController@delete');
+// });
+
 Route::get('campanhas', 'CampanhasController@index');
 Route::get('campanhas/{campanha}', 'CampanhasController@show');
 Route::post('campanhas', 'CampanhasController@store');
